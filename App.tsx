af78328032d4032b8c72cdc29ebd76b8136eab9f
Luis Cov Jr <shusten@hotmail.com>
2020-06-12 01:18:08 -0300

    :construction: chore: add home and assets

--- App.tsx
+++ App.tsx
@@ -1,19 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, View } from 'react-native';
+
+import Home from './src/pages/Home';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-    </View>
+    <>
+     <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+      <Home />
+    </>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+} 
