59e1a3c51fea8b51b9e8f93ec94923065c79d4b8
Luis Cov Jr <shusten@hotmail.com>
2020-06-13 23:52:30 -0300

    :construction: chore: add text and fonts

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import { View, Image, StyleSheet } from 'react-native'
+import { View, Text, Image, StyleSheet } from 'react-native'
 
 const Home = () => {
     return (
     <View style={styles.container}>
+      <View style={styles.main}>
         <Image source={require('../../assets/logo.png')} />
+        <Text style={styles.title}>Seu marketplace de coleta de resíduos</Text>
+        <Text style={styles.description}>Ajudamos pessoas a encontrarem pontos decoleta de forma eficiente.</Text>
+
+      </View>
     </View>
     );
 };
