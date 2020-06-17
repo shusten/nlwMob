56026505d6732f367828e58658d6fe41a216645f
Luis Cov Jr <shusten@hotmail.com>
2020-06-16 23:53:24 -0300

    :sparkles: feat: added Routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
-import { BaseNavigationContainer } from '@react-navigation/native';
+import { BaseNavigationContainer, NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Home from './pages/Home';
 import Points from './pages/Home';
 import Detail from './pages/Home';
+
+const AppStack = createStackNavigator();
+
+const Routes = () => {
+    return (
+        <NavigationContainer>
+            <AppStack.Navigator>
+            <AppStack.Screen name="Home" component={Home} />
+            <AppStack.Screen name="Points" component={Points} />
+            <AppStack.Screen name="Detail" component={Detail} />
+            </AppStack.Navigator>
+        </NavigationContainer>
+    );
+};
+
+export default Routes;
