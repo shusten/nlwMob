f5aa58f9a57229b4d10c6ab25f215e3e851de51d
Luis Cov Jr <shusten@hotmail.com>
2020-06-16 23:36:53 -0300

    :construction: add routes file

---
+++ src/routes.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { BaseNavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Home from './pages/Home';
