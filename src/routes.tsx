9ed76806323f4688dd77ca4cc4b64abc73f2dc0a
Luis Cov Jr <shusten@hotmail.com>
2020-06-16 23:42:21 -0300

    :construction: chore: add Points routes and Datails

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,3 +3,5 @@ import { BaseNavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Home from './pages/Home';
+import Points from './pages/Home';
+import Detail from './pages/Home';
